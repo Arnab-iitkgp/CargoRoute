05d84e3e25353e15bd168422306b030120167af5
Arnab-iitkgp <mrarnab7574@gmail.com>
2025-06-20 00:14:51 +0530

    created backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("DB error:", err));
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("VRP Optimizer Backend is Running");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
